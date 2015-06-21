0797a012e2f51978e5741acbee5e512ae3a0582b
enz <renz@posteo.de>
2015-06-21 18:56:47 +0200

    Initial commit of 1st review version

---
+++ footer.js
@@ -0,0 +1,74 @@
+document.write('\
+			<footer> \
+                <div class="container"> \
+                    <br> \
+                    <div class="main-footer"> \
+                        <div class="row"> \
+                            <div class="col-md-3"> \
+<!-- \
+                                <div class="about"> \
+                                    <h4 class="footer-title">Wollen auch Sie spenden?</h4> \
+                                    <p>Das Unternehmerforum freut sich über Spenden in jeglicher Höhe. Wenn Sie sich auch sozial engagieren wollen und anderen                                            Menschen dabei helfen wollen, dann können Sie uns entweder einen Scheck zukommen lassen oder direkt eine Überweisung                                              tätigen. Alle notwendigen Angaben finden Sie im Impressum. <br>Vielen Dank!</p> \
+                                </div> \
+--> \
+                            </div> \
+                            <div class="col-md-3"> \
+                                <div class="shop-list"> \
+                                    <h4 class="footer-title">Das könnte Sie interessieren:</h4> \
+                                    <ul> \
+                                        <li><a href="http://www.piding.de"><i class="fa fa-angle-right"></i>Gemeinde Piding</a></li> \
+                                        <li><a href="http://www.regiostar.com/"><i class="fa fa-angle-right"></i>RegioStar</a></li> \
+                                        <li><a href="http://www.pidinger-bienenweg.de/"><i class="fa fa-angle-right"></i>Bienenlehrpfad Piding</a></li> \
+                                        <li><a href="images/Satzung_UFOP.pdf"><i class="fa fa-angle-right"></i>Unsere Satzung</a></li> \
+                                    </ul> \
+                                </div> \
+                            </div> \
+                            <div class="col-md-3"> \
+                                <div class="recent-posts"> \
+                                    <h4 class="footer-title">Aktuelle Termine</h4> \
+                                    <div class="recent-post"> \
+                                        <div class="recent-post-thumb"> \
+                                            <img src="images/Haupttermin_Klein.jpg" alt=""> \
+                                        </div> \
+                                        <div class="recent-post-info"> \
+                                            <h6><a href="termine.html">Autofreier Sonntag</a></h6> \
+                                            <span>19. Juli 2015</span> \
+                                        </div> \
+                                    </div> \
+                                    <div class="recent-post"> \
+                                        <div class="recent-post-thumb"> \
+                                            <img src="images/NaechsterTermin.jpg" alt=""> \
+                                        </div> \
+                                        <div class="recent-post-info"> \
+                                            <h6><a href="termine.html">Mitglieder-Treffen</a></h6> \
+                                            <span>25. Juni 2015</span> \
+                                        </div> \
+                                    </div> \
+                                </div> \
+                            </div> \
+<!-- \
+                            <div class="col-md-3"> \
+                                <div class="more-info"> \
+                                     \
+                                    <h4 class="footer-title">Wo wir auch vertreten sind:</h4> \
+                                    <div class="social-bottom"> \
+                                    <ul> \
+                                        <li><a href="#" class="fa fa-facebook"></a></li> \
+                                        <li><a href="#" class="fa fa-twitter"></a></li> \
+                                        <li><a href="#" class="fa fa-rss"></a></li> \
+                                    </ul> \
+                                    </div> \
+                                    <p><a href="#">Impressum</a></p> \
+                                </div> \
+                            </div> \
+--> \
+                        </div> \
+                    </div> \
+                    <div class="bottom-footer"> \
+                        <p>Copyright © 2015 <a href="#">Jana Enzinger</a><br> \
+                        <a href="impressum.html"> Impressum</a></p> \
+                    </div> \
+ \
+                </div> \
+            </footer> \
+'); 
---
+++ header.js
@@ -0,0 +1,51 @@
+document.write('\
+<header> \
+  <div id="top-header"> \
+    <div class="container"> \
+      <div class="row"> \
+        <div class="col-md-6"> \
+          <div class="home-account"> \
+            <a href="#"></a> \
+            <a href="#"></a> \
+          </div> \
+        </div> \
+        <div class="col-md-6"> \
+           \
+        </div> \
+      </div> \
+    </div> \
+  </div> \
+  <div id="main-header"> \
+    <div class="container"> \
+      <div class="row"> \
+        <div class="col-md-3"> \
+          <div class="logo"> \
+            <a href="index.html"><img src="images/logo.png" title="Logo" alt="Logo" ></a> \
+          </div> \
+        </div> \
+        <div class="col-md-7"> \
+          <div class="main-menu"> \
+            <ul> \
+              <li><a href="index.html">Home</a></li> \
+              <li><a href="about-us.html">Das Forum</a></li> \
+              <li><a href="teilnehmer1.html">Mitglieder</a></li> \
+              <li><a href="termine.html">Termine</a></li> \
+              <li><a href="mailto:info@thumo.de">Kontakt</a></li> \
+            </ul> \
+          </div> \
+        </div> \
+        <!-- \
+            <div class="col-md-3"> \
+              <div class="search-box">   \
+                <form name="search_form" method="get" class="search_form"> \
+                  <input id="search" type="text" /> \
+                  <input type="submit" id="search-button" /> \
+                </form> \
+              </div> \
+            </div> \
+            --> \
+      </div> \
+    </div> \
+  </div> \
+</header> \
+'); 
